dfc94514025528b426c038d68048f5295dc4186d
Lucas Israel <lisraelalves92@gmail.com>
2023-10-24 14:31:51 -0300

    Museum

---
+++ src/main/java/com/betrybe/museumfinder/model/Museum.java
@@ -0,0 +1,13 @@
+package com.betrybe.museumfinder.model;
+
+public class Museum {
+  private long id;
+  private String description;
+  private String address;
+  private String collectionType;
+  private String subject;
+  private String url;
+  private Coordinate coordinate;
+  private long legacyId;
+
+}
