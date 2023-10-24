78048c579ece5398d2054a0505d67aec3e75fd39
Lucas Israel <lisraelalves92@gmail.com>
2023-10-24 14:37:52 -0300

    javadocs

--- src/main/java/com/betrybe/museumfinder/dto/MuseumCreationDto.java
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumCreationDto.java
@@ -2,6 +2,17 @@ package com.betrybe.museumfinder.dto;
 
 import com.betrybe.museumfinder.model.Coordinate;
 
+/**
+ * Usado para requisição de criação de museus.
+ *
+ * @param name do museu.
+ * @param description do museu.
+ * @param address do museu.
+ * @param collectionType do museu.
+ * @param subject do museu.
+ * @param url do museu.
+ * @param coordinate do museu.
+ */
 public record MuseumCreationDto(String name, String description, String address, String collectionType, String subject, String url, Coordinate coordinate) {
 
 }
--- src/main/java/com/betrybe/museumfinder/dto/MuseumDto.java
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumDto.java
@@ -2,6 +2,18 @@ package com.betrybe.museumfinder.dto;
 
 import com.betrybe.museumfinder.model.Coordinate;
 
+/**
+ * Usado como resposta da API.
+ *
+ * @param id do museu.
+ * @param name do museu.
+ * @param description do museu.
+ * @param address do museu.
+ * @param collectionType do museu.
+ * @param subject do museu.
+ * @param url do museu.
+ * @param coordinate do museu.
+ */
 public record MuseumDto(Long id, String name, String description, String address, String collectionType, String subject, String url, Coordinate coordinate) {
 
 }
