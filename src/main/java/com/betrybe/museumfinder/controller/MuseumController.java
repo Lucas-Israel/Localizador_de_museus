ec1c858ed96f3a6d74d133bb28c3cae7464b7daf
Lucas Israel <lisraelalves92@gmail.com>
2023-10-24 15:45:43 -0300

    MuseumController getClosestMuseum

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -1,14 +1,18 @@
 package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
+import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,5 +39,23 @@ public class MuseumController {
     Museum output = service.createMuseum(muse);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(output);
+  }
+
+  @GetMapping("/closest")
+  public ResponseEntity<MuseumDto> getClosestMuseum(@RequestParam double lat, double lng, Double max) {
+    Coordinate coord = new Coordinate(lat, lng);
+    Museum muse = service.getClosestMuseum(coord, max);
+    MuseumDto output = new MuseumDto(
+        muse.getId(),
+        muse.getName(),
+        muse.getDescription(),
+        muse.getAddress(),
+        muse.getCollectionType(),
+        muse.getSubject(),
+        muse.getUrl(),
+        muse.getCoordinate()
+    );
+
+    return ResponseEntity.ok(output);
   }
 }
