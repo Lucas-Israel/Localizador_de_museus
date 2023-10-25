eec2602bbe6ccd44b5da6efd1b249a6f8877ad70
Lucas Israel <lisraelalves92@gmail.com>
2023-10-25 13:05:44 -0300

    museumcontroller get museum/id

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,6 +57,23 @@ public class MuseumController {
         musee.getSubject(),
         musee.getUrl(),
         musee.getCoordinate()
+    );
+
+    return ResponseEntity.ok(output);
+  }
+
+  @GetMapping("museums/{id}")
+  public ResponseEntity<MuseumDto> getMuseumById(@PathVariable long id) {
+    Museum muse = service.getMuseum(id);
+    MuseumDto output = new MuseumDto(
+        muse.getId(),
+        muse.getName(),
+        muse.getDescription(),
+        muse.getAddress(),
+        muse.getCollectionType(),
+        muse.getSubject(),
+        muse.getUrl(),
+        muse.getCoordinate()
     );
 
     return ResponseEntity.ok(output);
